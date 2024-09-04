5da075029ea4d3f1122be7e907a84a1b052af41a
mkdiryuko <kobayashi.yuto164@mail.kyutech.jp>
2024-09-04 16:36:53 +0900

    add test.py

---
+++ src/test.py
@@ -0,0 +1,58 @@
+import cv2
+import time 
+
+# OpenCV face_recognition_model
+model_url = "/home/proken/workspace/proken_robot/assets/haarcascade_frontalface_default.xml"
+face_cascade = cv2.CascadeClassifier(model_url)
+
+if face_cascade.empty():
+    print("Error: Could not load Haar Cascade file.")
+
+url = "http://172.20.10.4:8080/video"
+
+cap = cv2.VideoCapture(url)
+
+ret, frame = cap.read()
+
+# cap center                                                            
+height, width = frame.shape[:2]
+cap_center_x = width // 2
+cap_center_y = height // 2
+
+last_faces = []
+
+last_detection_time = 0
+detection_interval = 2
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print('not read camera')
+        break
+
+    current_time = time.time()
+
+    if current_time - last_detection_time > detection_interval:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # face detection 
+        last_faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        last_detection_time = current_time 
+
+    if len(last_faces) > 0:
+        for (x, y, w, h) in last_faces:
+            # ROI内での座標をフレーム全体の座標に変換
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            center_x = x + w // 2
+            center_y = y + h // 2
+            cv2.circle(frame, (center_x, center_y), 2, (255, 0, 0), 2)
+            cv2.line(frame, (center_x, center_y),(cap_center_x, cap_center_y),(255, 0, 0),2)
+
+    cv2.imshow("Face Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
